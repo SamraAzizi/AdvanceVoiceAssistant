afa62ed136c8b4d2dd402d71e6f219dcd025547b
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-31 19:55:20 +0500

    advanceVoice

--- agent.py
+++ agent.py
@@ -9,9 +9,23 @@ from livekit.agents import(
 from livekit.agents.multimodal import MultimodalAgent
 from livekit.plugins import openai
 from dotenv import load_dotenv
+from api import AssistantFnc
 import os
 
 load_dotenv()
 
 async def entrypoint(ctx: JobContext):
     await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+
+    await ctx.wait_for_participant()
+
+    model = openai.realtime.RealtimeModel(
+        instruction="",
+        voice="shimmer",
+        temperature=0.8,
+        modalities=["audio", "text"]
+    )
+
+    assistant_fnc = AssistantFnc()
+    assistant = MultimodalAgent(model=model, fnc_ctx=assistant_fnc)
+    assistant.start(ctx.room)
