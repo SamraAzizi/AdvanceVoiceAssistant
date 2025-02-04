e6148ad87863d289e123261d13212ef493f043b6
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-04 22:49:19 +0500

    advanceVoiced

---
+++ frontend/src/components/livekitModel.jsx
@@ -0,0 +1,7 @@
+import { useState, useCallback } from "react";
+import {liveKitRoom, RoomAudioRender} from "@livekit/Commenta-react"
+import "@livekit/components-styles"
+
+const livekitModal = ({setShowSupport}) =>{
+    
+}
