3e50b691ec5e5581cfb5236e9ae1df478d371a63
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-04 23:01:39 +0500

    advanceVoiced

--- frontend/src/components/livekitModel.jsx
+++ frontend/src/components/livekitModel.jsx
@@ -5,8 +5,21 @@ import "@livekit/components-styles"
 const livekitModal = ({setShowSupport}) =>{
     const [isSubmittingName, setIsSubmittingName] = useState(false);
     const [name, setName] = useState("");
+    const handleNameSubmit = () =>{};
 
     return <div className="modal-overlay">
+        <div className="model-content">
+            <div className="support-room">
+                {isSubmittingName ? (
+                    <form onSubmit={handleNameSubmit} className="name-form">
+                        <h2>Enter Your Name to Connect With Support</h2>
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Your Name" required/>
+                    </form>
+                ) : <></>}
+
+            </div>
+
+        </div>
 
     </div>
 
