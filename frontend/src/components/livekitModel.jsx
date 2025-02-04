f63495eaa18f42542229e93c552fbf6d788a6da3
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-04 22:56:06 +0500

    advanceVoiced

--- frontend/src/components/livekitModel.jsx
+++ frontend/src/components/livekitModel.jsx
@@ -3,5 +3,11 @@ import {liveKitRoom, RoomAudioRender} from "@livekit/Commenta-react"
 import "@livekit/components-styles"
 
 const livekitModal = ({setShowSupport}) =>{
-    
+    const [isSubmittingName, setIsSubmittingName] = useState(false);
+    const [name, setName] = useState("");
+
+    return <div className="modal-overlay">
+
+    </div>
+
 }
