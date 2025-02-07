72edd8a9fd3b6ba95d283aa3d16b4f9c515bc65f
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-07 11:28:02 +0500

    advanceVoiced

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,37 @@
 import { useState } from 'react'
-import liveKitModal from './components/liveKitModal'
 import './App.css'
+import LiveKitModal from './components/LiveKitModal';
 
 function App() {
-
   const [showSupport, setShowSupport] = useState(false);
-  const handleSupportClick = () =>{
+
+  const handleSupportClick = () => {
     setShowSupport(true)
   }
-  
 
   return (
-   <div className='app'>
-    <header className='header'>
-      <div className='logo'>AutoZone</div>
+    <div className="app">
+      <header className="header">
+        <div className="logo">AutoZone</div>
+      </header>
 
-    </header>
-    <main>
-      <section className='hero'>
-        <h1>Get the Right Parts. Right Now</h1>
-        <p>Free Next Day Delivery on Eligible Orders</p>
-        <div className='seach-bar'>
-          <input type="text" placeholder='Enter Vehicle part number'/>
-          <button>Search</button>
-        </div>
-      </section>
-      <button className='support-button'onClick={handleSupportClick}></button>
-    </main>
-    {showSupport && <liveKitModal setShowSupport={handleSupportClick}/>}
-   </div>
-    
- 
+      <main>
+        <section className="hero">
+          <h1>Get the Right Parts. Right Now</h1>
+          <p>Free Next Day Delivery on Eligible Orders</p>
+          <div className="search-bar">
+            <input type="text" placeholder='Enter vehicle or part number'></input>
+            <button>Search</button>
+          </div>
+        </section>
+
+        <button className="support-button" onClick={handleSupportClick}>
+          Talk to an Agent!
+        </button>
+      </main>
+
+      {showSupport && <LiveKitModal setShowSupport={setShowSupport}/>}
+    </div>
   )
 }
 
